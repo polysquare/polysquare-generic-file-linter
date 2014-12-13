9e65084e7532237e2f998ffa23bbf65b48afd4e8
Sam Spilsbury <smspillaz@gmail.com>
2014-12-13 21:59:19 +0800

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,32 @@
 # Installation and setup script for polysquare-generic-file-linter
 #
 # See LICENCE.md for Copyright information
+"""Installation and setup script for polysquare-generic-file-linter"""
 
 from setuptools import setup, find_packages
 
-setup(name='polysquare-generic-file-linter',
-      version='0.0.5',
-      description='Polysquare Style Guide Linter',
-      long_description='Lint a file for the polysquare style guide',
-      author='Sam Spilsbury',
-      author_email='smspillaz@gmail.com',
-      classifiers=[
-           'Development Status :: 3 - Alpha',
-           'Intended Audience :: Developers',
-           'Topic :: Software Development :: Build Tools',
-           'License :: OSI Approved :: MIT License',
-           'Programming Language :: Python :: 3',
-      ],
-      license='MIT',
-      keywords='development linters',
-      packages=find_packages(exclude=['tests']),
+setup(name="polysquare-generic-file-linter",
+      version="0.0.6",
+      description="Polysquare Style Guide Linter",
+      long_description="Lint a file for the polysquare style guide",
+      author="Sam Spilsbury",
+      author_email="smspillaz@gmail.com",
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development :: Build Tools",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3"],
+      license="MIT",
+      keywords="development linters",
+      packages=find_packages(exclude=["tests"]),
+      install_requres=["setuptools"],
       extras_require={
-          'test': ['coverage']
+          "test": ["coverage",
+                   "testtools"]
       },
       entry_points={
-          'console_scripts': [
-              'polysquare-generic-file-linter=polysquarelinter.linter:main'
+          "console_scripts": [
+              "polysquare-generic-file-linter=polysquarelinter.linter:main"
           ]
       },
       test_suite="tests")
