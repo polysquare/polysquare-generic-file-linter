90baa1faede5197fb7ad30495d96afe91615c5f9
Sam Spilsbury <smspillaz@gmail.com>
2016-05-11 05:19:38 +0800

    polysquarelinter: Use decode() as opposed to encoding=
    It turns out that the former is only supported on python 3

--- polysquarelinter/lint_spelling_only.py
+++ polysquarelinter/lint_spelling_only.py
@@ -122,7 +122,7 @@ def main(arguments=None):  # suppress(unused-function)
     num_errors = 0
     for found_filename in result.files:
         file_path = os.path.abspath(found_filename)
-        with open(file_path, "r+", encoding="utf-8") as found_file:
+        with open(file_path, "r+") as found_file:
             jobstamps_dependencies = [file_path]
 
             if os.path.exists(dictionary_path):
@@ -138,7 +138,7 @@ def main(arguments=None):  # suppress(unused-function)
             }
 
             errors = jobstamp.run(spellcheck,
-                                  found_file.read(),
+                                  found_file.read().decode("utf-8"),
                                   result.technical_terms,
                                   result.spellcheck_cache,
                                   **kwargs)
--- polysquarelinter/linter.py
+++ polysquarelinter/linter.py
@@ -732,8 +732,8 @@ def _run_lint_on_file(file_path,
     If fix_what_you_can is specified, then the first error that has a
     possible replacement will be automatically fixed on this file.
     """
-    with open(file_path, "r+", encoding="utf-8") as found_file:
-        file_contents = found_file.read()
+    with open(file_path, "r+") as found_file:
+        file_contents = found_file.read().decode("utf-8")
         file_lines = file_contents.splitlines(True)
         try:
             errors = lint(file_path[len(os.getcwd()) + 1:],
