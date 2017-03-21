b7aa657007f1e10b5c83407d419f707281b94744
Sam Spilsbury <smspillaz@gmail.com>
2017-03-21 21:13:05 +0800

    spelling: Disable the spellcheck linter in ``` sections in comments
    Added this functionality to deal with the common case of
    having complicated code sections inside of comment blocks - in
    this case we might just want to disable the spellchecker alltogether
    and should allow the user to do so.

--- test/test_warnings.py
+++ test/test_warnings.py
@@ -529,6 +529,42 @@ class TestSpellingErrors(DisableStampingTestCase):
         self.assertTrue(result)
 
     @parameterized.expand(_KNOWN_STYLES)
+    def test_no_spelling_error_comments_blocked_region(self, style):
+        """No spelling errors where error is blocked out in comment."""
+        result = self._spellcheck_lint("{s}{e}\n{s} ```splelling```",
+                                       style)
+        self.assertTrue(result)
+
+    @parameterized.expand(_KNOWN_STYLES)
+    def test_no_spelling_error_comments_multiline_blocked_region(self, style):
+        """No spelling errors where error is multi-line blocked out."""
+        result = self._spellcheck_lint("{s}{e}\n"
+                                       "{s} ```splelling\n"
+                                       "{m} eror```{e}",
+                                       style)
+        self.assertTrue(result)
+
+    @parameterized.expand(_KNOWN_STYLES)
+    def test_no_persist_multiline_blocked_region(self, style):
+        """Don't persist multi-line blocked region."""
+        result = self._spellcheck_lint("{s}{e}\n"
+                                       "{s} ```splelling\n"
+                                       "{m} eror``` {e}\n"
+                                       "technical_term",
+                                       style)
+        self.assertTrue(result)
+
+    @parameterized.expand(_KNOWN_STYLES)
+    def test_no_persist_not_terminated_multiline_blocked_region(self, style):
+        """Don't persist multi-line blocked region, even if not terminated."""
+        result = self._spellcheck_lint("{s}{e}\n"
+                                       "{s} ```splelling\n"
+                                       "{m} eror {e}\n"
+                                       "technical_term",
+                                       style)
+        self.assertTrue(result)
+
+    @parameterized.expand(_KNOWN_STYLES)
     def test_no_spelling_error_inside_double_quoted_string(self, style):
         """No spelling errors inside single double-quote string."""
         result = self._spellcheck_lint("{s}{e}\n\"splelling\"",
