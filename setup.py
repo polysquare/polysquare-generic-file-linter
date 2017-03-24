7dcd182851b95826a651665ed2ccabe4c1cf3eeb
Travis-CI <travis@travis-ci.org>
2017-03-24 00:44:16 +0000

    Bump version: 0.1.22 -> 0.1.23
    [ci skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name="polysquare-generic-file-linter",
-      version="0.1.22",
+      version="0.1.23",
       description="Polysquare Style Guide Linter",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
